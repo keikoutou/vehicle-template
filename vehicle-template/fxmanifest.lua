f5575a40eeefbbeeaf754618e58dec3a9c6a48e3
keikoutou <ledkeikoutou0707@gmail.com>
2024-06-27 00:59:12 +0900

    fixed path

--- vehicle-template/fxmanifest.lua
+++ vehicle-template/fxmanifest.lua
@@ -21,6 +21,6 @@ data_file "HANDLING_FILE" "data/handling.meta"
 data_file "VEHICLE_METADATA_FILE" "data/vehicles.meta"
 data_file "CARCOLS_FILE" "data/carcols.meta"
 data_file "VEHICLE_VARIATION_FILE" "data/carvariations.meta"
-data_file "VEHICLE_LAYOUTS_FILE" "vehiclelayouts.meta"
+data_file "VEHICLE_LAYOUTS_FILE" "data/vehiclelayouts.meta"
 
 client_script "names.lua"
